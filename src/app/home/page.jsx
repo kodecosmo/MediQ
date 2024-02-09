52d39bac2164a327e1765047475bb5232dc087dd
Shakthi Warnakulasuriya <shakthi.20221274@iit.ac.lk>
2024-02-09 22:37:08 +0530

    landing page/ updated hero section

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,71 +1,103 @@
-import React from 'react';
-import Navlanding from '@/components/navLanding';
-import Link from 'next/link';
+"use client";
+
+import React from "react";
+import Navlanding from "@/components/navLanding";
+import Link from "next/link";
+import Spline from "@splinetool/react-spline";
 
 function Home() {
-    return (
-        <>
-            <Navlanding />
-            <div className="bg-background text-text">
-                {/* Hero Section */}
-                <section className="flex flex-col items-center justify-center h-screen px-4 pb-4 text-center">
-                    <h1 className="text-5xl font-bold  text-primary mb-4">Welcome to MedIQ</h1>
-                    <p className="text-xl mb-6">
-                        Your AI-powered first aid assistant.
-                    </p>
-                    <button className="bg-primary text-white py-2 px-4 rounded-lg hover:bg-accent transition">
-                        <Link href="/login">Get Started</Link>
-                    </button>
-                </section>
-
-                {/* Features Section */}
-                <section className="pb-12">
-                    <div className="max-w-4xl mx-auto px-4 pb-6">
-                        <h2 className="text-3xl font-bold text-center text-primary mb-12">
-                            Key Features
-                        </h2>
-                        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 group px-6">
-                            {/* Feature 1 */}
-                            <div className="p-6 border border-secondary rounded-lg group-hover:scale-[1] group-hover:hover:scale-105 transition duration-200 ease-in-out">
-                                <h3 className="text-2xl font-semibold mb-3">Real-Time Guidance</h3>
-                                <p>
-                                    Step-by-step first aid instructions tailored to specific emergencies.
-                                </p>
-                            </div>
-                            {/* Feature 2 */}
-                            <div className="p-6 border border-secondary rounded-lg group-hover:scale-[1] group-hover:hover:scale-105 transition duration-200 ease-in-out">
-                                <h3 className="text-2xl font-semibold mb-3">Symptom Analysis</h3>
-                                <p>
-                                    Advanced AI to analyze symptoms and suggest immediate actions.
-                                </p>
-                            </div>
-                            {/* Feature 2 */}
-                            <div className="p-6 border border-secondary rounded-lg group-hover:scale-[1] group-hover:hover:scale-105 transition duration-200 ease-in-out">
-                                <h3 className="text-2xl font-semibold mb-3">Natural Language processing</h3>
-                                <p>
-                                    Advanced AI to analyze symptoms and suggest immediate actions.
-                                </p>
-                            </div>
-                            {/* Feature 2 */}
-                            <div className="p-6 border border-secondary rounded-lg group-hover:scale-[1] group-hover:hover:scale-105 transition duration-200 ease-in-out">
-                                <h3 className="text-2xl font-semibold mb-3">Visual Analysis</h3>
-                                <p>
-                                    Advanced AI to analyze images using gemni pro vision and suggest immediate actions.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </section>
-
-                {/* Footer Section */}
-                <footer className="bg-indigo-700 text-white py-6">
-                    <div className="text-center">
-                        <p>kode &copy; {new Date().getFullYear()}</p>
-                    </div>
-                </footer>
+  return (
+    <>
+      <Navlanding />
+      <div className="bg-background text-text">
+        {/* Hero Section */}
+        <section className="flex items-center max-w-7xl m-auto justify-start h-screen px-8 pb-4 gap-10">
+          <div className="w-2/3 mt-28 flex-col">
+            <div className="w-full flex flex-col justify-center items-start">
+              <h1 className="text-5xl font-bold  text-primary mb-4">
+                Be Prepaired <br /> Anytime Anywhere
+              </h1>
+              <p className="text-xl mb-6">
+                With Your AI-powered First-aid Assistant That is in your pocket
+                whereever you go so that you can be a helping hand whenever the
+                times call for it. Go and be a hero
+              </p>
             </div>
-        </>
-    )
+
+            <div className="flex w-full justify-start items-center gap-5">
+              <button className="bg-primary text-white py-3 px-5 rounded-lg hover:bg-accent transition">
+                <Link href="/login">Try MedIQ</Link>
+              </button>
+              <button className="border-2 border-primary px-4 py-2 rounded-lg">
+                <Link href="/login">Watch Intro Video</Link>
+              </button>
+            </div>
+          </div>
+
+          <div className="w-1/3 mt-28 flex justify-end items-center h-full mx-auto">
+            <Spline scene="https://prod.spline.design/MOfJtci1RXwfyvf3/scene.splinecode" />
+          </div>
+        </section>
+
+        {/* Features Section */}
+        <section className="mt-28 h-screen max-w-7xl mx-auto"></section>
+
+        {/* Our Team Section */}
+        <section className="mt-28 h-screen">
+          <div className="max-w-4xl mx-auto px-4 pb-6">
+            <h2 className="text-3xl font-bold text-center text-primary mb-12">
+              Key Features
+            </h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 group px-6">
+              {/* Feature 1 */}
+              <div className="p-6 border border-secondary rounded-lg group-hover:scale-[1] group-hover:hover:scale-105 transition duration-200 ease-in-out">
+                <h3 className="text-2xl font-semibold mb-3">
+                  Real-Time Guidance
+                </h3>
+                <p>
+                  Step-by-step first aid instructions tailored to specific
+                  emergencies.
+                </p>
+              </div>
+              {/* Feature 2 */}
+              <div className="p-6 border border-secondary rounded-lg group-hover:scale-[1] group-hover:hover:scale-105 transition duration-200 ease-in-out">
+                <h3 className="text-2xl font-semibold mb-3">
+                  Symptom Analysis
+                </h3>
+                <p>
+                  Advanced AI to analyze symptoms and suggest immediate actions.
+                </p>
+              </div>
+              {/* Feature 2 */}
+              <div className="p-6 border border-secondary rounded-lg group-hover:scale-[1] group-hover:hover:scale-105 transition duration-200 ease-in-out">
+                <h3 className="text-2xl font-semibold mb-3">
+                  Natural Language processing
+                </h3>
+                <p>
+                  Advanced AI to analyze symptoms and suggest immediate actions.
+                </p>
+              </div>
+              {/* Feature 2 */}
+              <div className="p-6 border border-secondary rounded-lg group-hover:scale-[1] group-hover:hover:scale-105 transition duration-200 ease-in-out">
+                <h3 className="text-2xl font-semibold mb-3">Visual Analysis</h3>
+                <p>
+                  Advanced AI to analyze images using gemni pro vision and
+                  suggest immediate actions.
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        {/* Footer Section */}
+        <footer className="bg-indigo-700 text-white py-6">
+          <div className="text-center">
+            <p>kode &copy; {new Date().getFullYear()}</p>
+          </div>
+        </footer>
+      </div>
+    </>
+  );
 }
 
 export default Home;
--- src/components/navLanding.jsx
+++ src/components/navLanding.jsx
@@ -16,81 +16,83 @@ function Navlanding({}) {
   const appName = process.env.NEXT_PUBLIC_APP_NAME;
 
   return (
-    <nav className="bg-background border-b py-5 md:py-4">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between items-center">
-          <div className="flex justify-between items-center w-full lg:w-auto">
-            <Link href="/home">
-              <div className="font-bold text-xl text-primary cursor-pointer">
-                {appName}
-              </div>
-            </Link>
-            <div
-              className="text-3xl absolute right-8 top-5 cursor-pointer md:hidden"
-              onClick={toggleMenu}
-            >
-              <Menu />
-            </div>
-          </div>
-
-          <div className="hidden md:flex items-baseline space-x-4">
-            <Link href="/about">
-              <div className="text-primary hover:text-blue-900 px-3 py-2 rounded-md text-base font-medium cursor-pointer">
-                About
-              </div>
-            </Link>
-            <Link href="/services">
-              <div className="text-primary hover:text-blue-900 px-3 py-2 rounded-md text-base font-medium cursor-pointer">
-                Services
-              </div>
-            </Link>
-            <Link href="/contact">
-              <div className="text-primary hover:text-blue-900 px-3 py-2 rounded-md text-base font-medium cursor-pointer">
-                Contact
-              </div>
-            </Link>
-          </div>
-
-          <div className="hidden md:flex">
-            <Link href="/login">
-              <div className="text-primary hover:text-blue-900 px-3 py-2 rounded-md text-base font-medium cursor-pointer">
-                Login
-              </div>
-            </Link>
-          </div>
-
-          <AnimatePresence>
-            {isOpen && (
-              <motion.div
-                className="absolute right-0 h-[200px] rounded-lg shadow-sm  mr-6 border w-1/3 bg-background flex flex-col items-center justify-center md:hidden"
-                variants={menuVariants}
-                initial="closed"
-                animate="open"
-                exit="closed"
+    <nav className="w-full fixed">
+      <div className="bg-background border-b py-5 md:py-4">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="flex justify-between items-center">
+            <div className="flex justify-between items-center w-full lg:w-auto">
+              <Link href="/home">
+                <div className="font-bold text-xl text-primary cursor-pointer">
+                  {appName}
+                </div>
+              </Link>
+              <div
+                className="text-3xl absolute right-8 top-5 cursor-pointer md:hidden"
+                onClick={toggleMenu}
               >
-                <Link href="/about">
-                  <div className="text-accent px-3 py-2 rounded-md text-base font-medium cursor-pointer">
-                    About
-                  </div>
-                </Link>
-                <Link href="/services">
-                  <div className="text-accent px-3 py-2 rounded-md text-base font-medium cursor-pointer">
-                    Services
-                  </div>
-                </Link>
-                <Link href="/contact">
-                  <div className="text-accent px-3 py-2 rounded-md text-base font-medium cursor-pointer">
-                    Contact
-                  </div>
-                </Link>
-                <Link href="/login">
-                  <div className="text-accent px-3 py-2 rounded-md text-base font-medium cursor-pointer">
-                    Login
-                  </div>
-                </Link>
-              </motion.div>
-            )}
-          </AnimatePresence>
+                <Menu />
+              </div>
+            </div>
+
+            <div className="hidden md:flex items-baseline space-x-4">
+              <Link href="/about">
+                <div className="text-primary hover:text-blue-900 px-3 py-2 rounded-md text-base font-medium cursor-pointer">
+                  About
+                </div>
+              </Link>
+              <Link href="/services">
+                <div className="text-primary hover:text-blue-900 px-3 py-2 rounded-md text-base font-medium cursor-pointer">
+                  Services
+                </div>
+              </Link>
+              <Link href="/contact">
+                <div className="text-primary hover:text-blue-900 px-3 py-2 rounded-md text-base font-medium cursor-pointer">
+                  Contact
+                </div>
+              </Link>
+            </div>
+
+            <div className="hidden md:flex">
+              <Link href="/login">
+                <div className="text-primary hover:text-blue-900 px-3 py-2 rounded-md text-base font-medium cursor-pointer">
+                  Login
+                </div>
+              </Link>
+            </div>
+
+            <AnimatePresence>
+              {isOpen && (
+                <motion.div
+                  className="absolute right-0 h-[200px] rounded-lg shadow-sm  mr-6 border w-1/3 bg-background flex flex-col items-center justify-center md:hidden"
+                  variants={menuVariants}
+                  initial="closed"
+                  animate="open"
+                  exit="closed"
+                >
+                  <Link href="/about">
+                    <div className="text-accent px-3 py-2 rounded-md text-base font-medium cursor-pointer">
+                      About
+                    </div>
+                  </Link>
+                  <Link href="/services">
+                    <div className="text-accent px-3 py-2 rounded-md text-base font-medium cursor-pointer">
+                      Services
+                    </div>
+                  </Link>
+                  <Link href="/contact">
+                    <div className="text-accent px-3 py-2 rounded-md text-base font-medium cursor-pointer">
+                      Contact
+                    </div>
+                  </Link>
+                  <Link href="/login">
+                    <div className="text-accent px-3 py-2 rounded-md text-base font-medium cursor-pointer">
+                      Login
+                    </div>
+                  </Link>
+                </motion.div>
+              )}
+            </AnimatePresence>
+          </div>
         </div>
       </div>
     </nav>
